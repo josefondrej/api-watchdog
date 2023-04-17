94c9cfa35f54a18777c34a28f578d9d64d05b07e
Josef Ondrej <josef.ondrej@outlook.com>
2023-04-17 10:39:21 +0200

    Add request_frequency_sec parameter to Config

--- api_watchdog/core/config.py
+++ api_watchdog/core/config.py
@@ -5,21 +5,30 @@ from api_watchdog.core.serializable import Serializable
 
 
 class Config(Serializable):
-    def __init__(self, api_test_cases: Optional[List[ApiTestCase]] = None):
+    def __init__(self, api_test_cases: Optional[List[ApiTestCase]] = None, request_frequency_sec: int = 60):
         self._api_test_cases = api_test_cases or []
+        self._request_frequency_sec = int(request_frequency_sec)
 
     @property
     def api_test_cases(self) -> List[ApiTestCase]:
         return self._api_test_cases
+
+    @property
+    def request_frequency_sec(self) -> int:
+        return self._request_frequency_sec
 
     def add_api_test_case(self, api_test_case: ApiTestCase):
         self._api_test_cases.append(api_test_case)
 
     def to_dict(self) -> Dict:
         return {
-            'api_test_cases': [api_test_case.to_dict() for api_test_case in self._api_test_cases]
+            'api_test_cases': [api_test_case.to_dict() for api_test_case in self._api_test_cases],
+            'request_frequency_sec': self._request_frequency_sec
         }
 
     @classmethod
     def from_dict(cls, data: Dict) -> 'Config':
-        return cls(api_test_cases=[ApiTestCase.from_dict(api_test_case) for api_test_case in data['api_test_cases']])
+        return cls(
+            api_test_cases=[ApiTestCase.from_dict(api_test_case_data) for api_test_case_data in data['api_test_cases']],
+            request_frequency_sec=data['request_frequency_sec']
+        )
