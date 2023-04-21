f0e7673bdc3f330e778fc59182385099465e43fc
Josef Ondrej <josef.ondrej@outlook.com>
2023-04-21 17:23:49 +0200

    Fix database and config file paths

--- api_watchdog/config.py
+++ api_watchdog/config.py
@@ -1,5 +1,5 @@
 import os
 
 DATA_DIR = os.environ.get('API_WATCHDOG_DATA_DIR', '/tmp')
-DATABASE_CONNECTION_STRING = f'sqlite:///{DATA_DIR / "database.db"}'
-CONFIG_FILE_PATH = DATA_DIR / 'config.json'
+DATABASE_CONNECTION_STRING = f'sqlite:///{DATA_DIR}/database.db'
+CONFIG_FILE_PATH = f'{DATA_DIR}/config.json'
