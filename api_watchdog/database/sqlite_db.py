ba3a97539cb02895b49cd902d0fb5a30ec2fb876
Josef Ondrej <josef.ondrej@outlook.com>
2023-04-21 16:57:27 +0200

    Use parameters for inserting to the db

--- api_watchdog/database/sqlite_db.py
+++ api_watchdog/database/sqlite_db.py
@@ -28,19 +28,30 @@ class SqliteDb(DbBase):
             connection.execute(text(create_api_test_case_records_table_sql))
 
     def insert_api_test_case_record(self, api_test_case_record: ApiTestCaseRecord):
+        api_test_case_identifier = api_test_case_record.result.api_test_case_identifier
+        serialized_api_test_case_record = json.dumps(api_test_case_record.to_dict())
+
         sql = f"INSERT INTO api_test_case_records (api_test_case_identifier, serialized_api_test_case_record) " \
-              f"VALUES ('{api_test_case_record.result.api_test_case_identifier}', " \
-              f"'{json.dumps(api_test_case_record.to_dict())}')"
+              f"VALUES (:api_test_case_identifier, :serialized_api_test_case_record)"
         sql_executable = text(sql)
 
         with self._engine.connect() as connection:
-            connection.execute(sql_executable)
+            connection.execute(
+                statement=sql_executable,
+                parameters=dict(
+                    api_test_case_identifier=api_test_case_identifier,
+                    serialized_api_test_case_record=serialized_api_test_case_record
+                )
+            )
             connection.commit()
 
     def list_api_test_case_records(self, api_test_case_identifier: str) -> List[ApiTestCaseRecord]:
-        sql = f"SELECT serialized_api_test_case_record FROM api_test_case_records WHERE api_test_case_identifier = '{api_test_case_identifier}'"
+        sql = f"SELECT serialized_api_test_case_record FROM api_test_case_records WHERE api_test_case_identifier = :api_test_case_identifier"
         sql_executable = text(sql)
 
         with self._engine.connect() as connection:
-            results = connection.execute(sql_executable).fetchall()
+            results = connection.execute(
+                statement=sql_executable,
+                parameters=dict(api_test_case_identifier=api_test_case_identifier)
+            ).fetchall()
             return [ApiTestCaseRecord.from_dict(json.loads(result[0])) for result in results]
