a755b7ddd312a7d4ae9f3003786c91452938d5bd
Josef Ondrej <josef.ondrej@outlook.com>
2023-04-21 15:07:13 +0200

    Add db to monitoring

--- api_watchdog/config.py
+++ api_watchdog/config.py
@@ -1,10 +1,5 @@
-import logging
 from pathlib import Path
 
 DATA_DIR = Path(__file__).parent.parent / 'data'
 DATABASE_CONNECTION_STRING = f'sqlite:///{DATA_DIR / "database.db"}'
 CONFIG_FILE_PATH = DATA_DIR / 'config.json'
-
-logger = logging.getLogger(__name__)
-logger.info(f'Data dir: {DATA_DIR}')
-logger.info(f'Database connection string: {DATABASE_CONNECTION_STRING}')
--- api_watchdog/monitoring/main.py
+++ api_watchdog/monitoring/main.py
@@ -3,15 +3,20 @@ import time
 from pathlib import Path
 from typing import Union
 
+from sqlalchemy import create_engine
+
+import api_watchdog.config as config
+from api_watchdog.core.api_test_case_result_status import ApiTestCaseResultStatus
 from api_watchdog.core.config import Config
 from api_watchdog.core.main import run_api_test_case
 from api_watchdog.core.utils import read_json_atomic
+from api_watchdog.database.sqlite_db import SqliteDb
 
 logger = logging.getLogger(__name__)
 
 
-def run_monitoring(config_file_path: Union[Path, str]):
-    logger.info(f'Starting monitoring with config file: {config_file_path}')
+def run_monitoring(config_file_path: Union[Path, str], database: SqliteDb):
+    logger.info(f'Starting monitoring; Config file: {config_file_path}; Database: {database}')
     while True:
         logger.info('Reading config file')
         config = Config.from_dict(read_json_atomic(config_file_path))
@@ -19,7 +24,10 @@ def run_monitoring(config_file_path: Union[Path, str]):
         logger.info('Running api test cases')
         for api_test_case in config.api_test_cases:
             api_test_case_record = run_api_test_case(api_test_case)
-            print(api_test_case_record)  # TODO: Save to database
+            database.insert_api_test_case_record(api_test_case_record)
+            if api_test_case_record.result.status != ApiTestCaseResultStatus.PASSED:
+                logger.error(f'Api test case failed: {api_test_case_record.to_dict()}')
+                # TODO: Connect to some notification system here
 
         logger.info(f'Waiting {config.request_frequency_sec} seconds')
         time.sleep(config.request_frequency_sec)
@@ -27,6 +35,6 @@ def run_monitoring(config_file_path: Union[Path, str]):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    import api_watchdog.config as config
-
-    run_monitoring(config_file_path=config.CONFIG_FILE_PATH)
+    sqlite_engine = create_engine(config.DATABASE_CONNECTION_STRING)
+    database = SqliteDb(sqlite_engine)
+    run_monitoring(config_file_path=config.CONFIG_FILE_PATH, database=database)
